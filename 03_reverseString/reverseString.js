98f7d6028118aabca2d957357db018dfb9043f86
linusekman <linus.ekman@outlook.com>
2024-03-25 11:17:02 +0100

    Added solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(words) {
+//    let splitWords = words.split("");
+//    let reverseWords = splitWords.reverse();
+//    let joinWords = reverseWords.join("");
 
+//    return joinWords;
+
+    return words.split("").reverse().join("");
 };
 
 // Do not edit below this line
